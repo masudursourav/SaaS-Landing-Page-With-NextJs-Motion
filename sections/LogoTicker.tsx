32eb4eae8390f2dd6a1373e4ece1359ca839378c
Masudur Rahman Sourav <it19025@mbstu.ac.bd>
2025-07-01 08:11:39 +0600

    motion added to the companies logo

--- sections/LogoTicker.tsx
+++ sections/LogoTicker.tsx
@@ -1,16 +1,29 @@
+"use client";
 import AcmeLogo from "@/assets/logo-acme.png";
 import ApexLogo from "@/assets/logo-apex.png";
 import CelestialLogo from "@/assets/logo-celestial.png";
 import EchoLogo from "@/assets/logo-echo.png";
 import PulseLogo from "@/assets/logo-pulse.png";
 import QuantumLogo from "@/assets/logo-quantum.png";
+import { motion } from "framer-motion";
 import Image from "next/image";
 export const LogoTicker = () => {
   return (
     <section className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 15,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
             <Image src={AcmeLogo} alt="Acme Logo" className="h-8 w-auto" />
             <Image
               src={CelestialLogo}
@@ -25,7 +38,22 @@ export const LogoTicker = () => {
               alt="Quantum Logo"
               className="h-8 w-auto"
             />
-          </div>
+
+            <Image src={AcmeLogo} alt="Acme Logo" className="h-8 w-auto" />
+            <Image
+              src={CelestialLogo}
+              alt="Celestial Logo"
+              className="h-8 w-auto"
+            />
+            <Image src={ApexLogo} alt="Apex Logo" className="h-8 w-auto" />
+            <Image src={EchoLogo} alt="Echo Logo" className="h-8 w-auto" />
+            <Image src={PulseLogo} alt="Pulse Logo" className="h-8 w-auto" />
+            <Image
+              src={QuantumLogo}
+              alt="Quantum Logo"
+              className="h-8 w-auto"
+            />
+          </motion.div>
         </div>
       </div>
     </section>
